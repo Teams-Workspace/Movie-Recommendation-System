cf9cfc6140eff56459431b934ac39b66801edf8b
Saad123ali <itmesaad@gmail.com>
2024-11-02 16:17:23 +0500

    Quick Sort Apply

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,20 +1,39 @@
 // controllers/movieController.js
 const movieModel = require('../models/movieModel');
 
-
+// Search for movies based on the query
 exports.searchMovies = (req, res) => {
     const searchTerm = req.query.q || '';
+    const searchType = req.query.searchType || 'quickSort'; // Default to Quick Sort
 
-    movieModel.searchMovies(searchTerm, (err, movies) => {
+    movieModel.searchMovies(searchTerm, searchType, (err, movies) => {
         if (err) {
             console.error('Error fetching movies:', err);
-            return res.status(500).send('Server Error');
+            return res.status(500).render('500', { message: 'Internal Server Error' });
         }
 
-        // Assuming you want to find the first movie as the searched movie
-        const searchedMovie = movies.length > 0 ? movies[0] : null;
+        res.render('searchResults', { searchTerm, movies });
+    });
+};
 
-        res.render('movies', { searchedMovie, movies }); // Pass both searchedMovie and movies array
+// controllers/movieController.js
+
+exports.getMovieDetails = (req, res) => {
+    const movieId = req.query.moviesdetails;
+    console.log("Movie ID received:", movieId); // Log the received movie ID
+
+    movieModel.getMovieById(movieId, (err, movie) => {
+        if (err) {
+            console.error('Error fetching movie details:', err);
+            return res.status(500).render('500', { message: 'Internal Server Error' });
+        }
+
+        if (!movie) {
+            return res.status(404).render('404', { message: 'Movie not found' });
+        }
+
+        console.log("Movie details found:", movie); // Log the fetched movie details
+        res.render('movieDetails', { movie });
     });
 };
 
