26d18cb2bd8cb1dac7c2416873e4185294bb29ac
itsmesaadali <itmesaad@gmail.com>
2025-07-13 16:08:00 +0500

    add frontend link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 DB();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL
+  origin: 'https://mrsapp.netlify.app', // frontend URL
   credentials: true
 }));
 
